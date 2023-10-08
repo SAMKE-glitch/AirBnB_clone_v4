addf133b1282321fc63a9efb0069969068e33e3f
Sue609 <122805048+Sue609@users.noreply.github.com>
2023-10-08 19:51:16 +0300

    Web dynamic implementation.

--- web_dynamic/1-hbnb.py
+++ web_dynamic/1-hbnb.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+"""
+Starts a Flash Web Application
+"""
 from models import storage
 from models.state import State
 from models.city import City
@@ -7,6 +9,7 @@ from models.amenity import Amenity
 from models.place import Place
 from os import environ
 from flask import Flask, render_template
+import uuid
 app = Flask(__name__)
 
 
@@ -16,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/0-hbnb/', strict_slashes=False)
+@app.route('/1-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -32,10 +35,11 @@ def hbnb():
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
 
-    return render_template('100-hbnb.html',
+    return render_template('1-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
-                           places=places)
+                           places=places,
+                           cache_id=str(uuid.uuid4()))
 
 
 if __name__ == "__main__":
